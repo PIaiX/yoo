b62cf9645039c12a0c0da555beb2e0e270f42890
Gikami <gikami.book@gmail.com>
2023-09-13 12:21:31 +0300

    Create Policy.jsx

---
+++ src/pages/Policy.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useLayoutEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Loader from "../components/utils/Loader";
+import { getDocument } from "../services/document";
+
+const Policy = () => {
+  const [document, setDocument] = useState({ loading: true, data: false });
+
+  useLayoutEffect(() => {
+    getDocument("policy")
+      .then((res) => setDocument({ loading: false, data: res }))
+      .catch(() => setDocument((data) => ({ ...data, loading: false })));
+  }, []);
+
+  if (document?.loading) {
+    return <Loader full />;
+  }
+
+  return (
+    <main>
+      <section className="sec-7 mb-5">
+        <Container>
+          {document?.data?.content && (
+            <div dangerouslySetInnerHTML={{ __html: document.data.content }} />
+          )}
+        </Container>
+      </section>
+    </main>
+  );
+};
+
+export default Policy;
