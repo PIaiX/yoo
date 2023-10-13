f5373db541f4d986c36af5507c2af87e8ac88454
gikami <gikami.book@gmail.com>
2023-10-13 16:43:37 +0300

    Update Registration.jsx

--- src/pages/auth/Registration.jsx
+++ src/pages/auth/Registration.jsx
@@ -8,6 +8,7 @@ import { authRegister, login } from "../../services/auth";
 import Meta from "../../components/Meta";
 import { Button } from "react-bootstrap";
 import { NotificationManager } from "react-notifications";
+import { isMobile } from "react-device-detect";
 
 const Registration = () => {
   const isAuth = useSelector((state) => state.auth.isAuth);
@@ -15,6 +16,7 @@ const Registration = () => {
   const options = useSelector((state) => state.settings.options);
 
   const navigate = useNavigate();
+
   const [loginView, setLoginView] = useState(true);
   const block1 = useRef();
   const block2 = useRef();
@@ -139,243 +141,245 @@ const Registration = () => {
     setTimeout(() => setLoginView(!loginView), 500);
   };
 
+  const Reg = () => (
+    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <h4 class="main-color text-center fw-6">Свозвращением!</h4>
+      <p class="text-center fs-11 mb-5">Вкусные роллы ипицца скучали потебе</p>
+      <div className="mb-3">
+        {!options.authType || options.authType === "email" ? (
+          <Input
+            type="email"
+            label="Email"
+            name="email"
+            placeholder="Введите email"
+            errors={errors}
+            register={register}
+            validation={{
+              required: "Введите email",
+              maxLength: {
+                value: 250,
+                message: "Максимально 250 символов",
+              },
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Неверный формат Email",
+              },
+            }}
+          />
+        ) : (
+          <Input
+            type="custom"
+            label="Номер телефона"
+            name="phone"
+            placeholder="+7(900)000-00-00"
+            mask="+7(999)999-99-99"
+            errors={errors}
+            register={register}
+            maxLength={16}
+            validation={{
+              required: "Введите номер телефона",
+              maxLength: {
+                value: 16,
+                message: "Максимально 16 символов",
+              },
+            }}
+          />
+        )}
+      </div>
+      <div className="mb-3">
+        <Input
+          label="Пароль"
+          type="password"
+          name="password"
+          errors={errors}
+          placeholder="Введите пароль"
+          register={register}
+          validation={{
+            required: "Введите пароль",
+            minLength: {
+              value: 6,
+              message: "Минимальный пароль должен состоять из 6 символов",
+            },
+          }}
+        />
+      </div>
+      <Button
+        type="submit"
+        variant="primary"
+        disabled={!isValid}
+        className="w-100 rounded-3"
+      >
+        Войти
+      </Button>
+      <div className="mt-4 text-center text-muted fs-09">
+        <Link to="/recovery">Забыли пароль?</Link>
+      </div>
+    </form>
+  );
+
+  const Login = () => (
+    <form className="login-form" onSubmit={handleSubmitReg(onSubmitReg)}>
+      <h4 class="main-color text-center fw-6">Привет, друг!</h4>
+      <p class="text-center fs-11 mb-5">
+        Введи данные чтобы зарегистрироваться
+      </p>
+      <div className="mb-3">
+        {!options.authType || options.authType === "email" ? (
+          <Input
+            type="email"
+            label="Email"
+            name="email"
+            placeholder="Введите email"
+            errors={errorsReg}
+            register={registerReg}
+            validation={{
+              required: "Введите email",
+              maxLength: {
+                value: 250,
+                message: "Максимально 250 символов",
+              },
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Неверный формат Email",
+              },
+            }}
+          />
+        ) : (
+          <Input
+            type="custom"
+            label="Номер телефона"
+            name="phone"
+            placeholder="+7(900)000-00-00"
+            mask="+7(999)999-99-99"
+            errors={errorsReg}
+            register={registerReg}
+            maxLength={16}
+            validation={{
+              required: "Введите номер телефона",
+              maxLength: {
+                value: 16,
+                message: "Максимально 16 символов",
+              },
+            }}
+          />
+        )}
+      </div>
+      <div className="mb-3">
+        <Input
+          type="password"
+          label="Пароль"
+          placeholder="Придумайте пароль"
+          name="password"
+          errors={errorsReg}
+          register={registerReg}
+          validation={{
+            required: "Введите пароль",
+            minLength: {
+              value: 6,
+              message: "Минимальное кол-во символов 6",
+            },
+            maxLength: {
+              value: 250,
+              message: "Максимальное кол-во символов 250",
+            },
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <Input
+          type="password"
+          label="Подтверждение пароля"
+          placeholder="Повторите пароль"
+          name="passwordConfirm"
+          errors={errorsReg}
+          register={registerReg}
+          validation={{
+            required: "Введите повторный пароль",
+            minLength: {
+              value: 6,
+              message: "Минимальное кол-во символов 6",
+            },
+            maxLength: {
+              value: 250,
+              message: "Максимальное кол-во символов 250",
+            },
+          }}
+        />
+      </div>
+      <label className="d-flex pale-blue mb-3">
+        <input
+          type="checkbox"
+          className="checkbox me-2"
+          {...registerReg("accept", {
+            required: "Примите условия пользовательского соглашения",
+          })}
+        />
+        <span className="fs-09">
+          Принять условия Пользовательского соглашения
+        </span>
+      </label>
+      <Button
+        type="submit"
+        variant="primary"
+        disabled={!isValidReg}
+        className="w-100 rounded-3"
+      >
+        Зарегистрироваться
+      </Button>
+    </form>
+  );
+
   return (
     <main className="py-lg-0">
       <Meta title={loginView ? "Регистрация" : "Вход"} />
       <Container>
-        <section className="d-none d-lg-block login">
-          <div ref={block2} className="login-forms">
+        {isMobile ? (
+          <section className="d-lg-none login-mobile">
+            {loginView ? <Reg /> : <Login />}
             {loginView ? (
-              <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                <h4 class="main-color text-center fw-6">Свозвращением!</h4>
-                <p class="text-center fs-11 mb-5">
-                  Вкусные роллы ипицца скучали потебе
-                </p>
-                <div className="mb-3">
-                  {!options.authType || options.authType === "email" ? (
-                    <Input
-                      type="email"
-                      label="Email"
-                      name="email"
-                      placeholder="Введите email"
-                      errors={errors}
-                      register={register}
-                      validation={{
-                        required: "Введите email",
-                        maxLength: {
-                          value: 250,
-                          message: "Максимально 250 символов",
-                        },
-                        pattern: {
-                          value: /\S+@\S+\.\S+/,
-                          message: "Неверный формат Email",
-                        },
-                      }}
-                    />
-                  ) : (
-                    <Input
-                      type="custom"
-                      label="Номер телефона"
-                      name="phone"
-                      placeholder="+7(900)000-00-00"
-                      mask="+7(999)999-99-99"
-                      errors={errors}
-                      register={register}
-                      maxLength={16}
-                      validation={{
-                        required: "Введите номер телефона",
-                        maxLength: {
-                          value: 16,
-                          message: "Максимально 16 символов",
-                        },
-                      }}
-                    />
-                  )}
-                </div>
-                <div className="mb-3">
-                  <Input
-                    label="Пароль"
-                    type="password"
-                    name="password"
-                    errors={errors}
-                    placeholder="Введите пароль"
-                    register={register}
-                    validation={{
-                      required: "Введите пароль",
-                      minLength: {
-                        value: 6,
-                        message:
-                          "Минимальный пароль должен состоять из 6 символов",
-                      },
-                    }}
-                  />
-                </div>
-                <Button
-                  type="submit"
-                  variant="primary"
-                  disabled={!isValid}
-                  className="w-100 rounded-3"
-                >
-                  Войти
-                </Button>
-                <div className="mt-4 text-center text-muted fs-09">
-                  <Link to="/recovery">Забыли пароль?</Link>
-                </div>
-              </form>
-            ) : (
-              <form
-                className="login-form"
-                onSubmit={handleSubmitReg(onSubmitReg)}
+              <button
+                type="button"
+                onClick={() => setLoginView(false)}
+                className="main-color fs-13 mx-auto mt-4 text-decoration-underline"
               >
-                <h4 class="main-color text-center fw-6">Привет, друг!</h4>
-                <p class="text-center fs-11 mb-5">
-                  Введи данные чтобы зарегистрироваться
-                </p>
-                <div className="mb-3">
-                  {!options.authType || options.authType === "email" ? (
-                    <Input
-                      type="email"
-                      label="Email"
-                      name="email"
-                      placeholder="Введите email"
-                      errors={errorsReg}
-                      register={registerReg}
-                      validation={{
-                        required: "Введите email",
-                        maxLength: {
-                          value: 250,
-                          message: "Максимально 250 символов",
-                        },
-                        pattern: {
-                          value: /\S+@\S+\.\S+/,
-                          message: "Неверный формат Email",
-                        },
-                      }}
-                    />
-                  ) : (
-                    <Input
-                      type="custom"
-                      label="Номер телефона"
-                      name="phone"
-                      placeholder="+7(900)000-00-00"
-                      mask="+7(999)999-99-99"
-                      errors={errorsReg}
-                      register={registerReg}
-                      maxLength={16}
-                      validation={{
-                        required: "Введите номер телефона",
-                        maxLength: {
-                          value: 16,
-                          message: "Максимально 16 символов",
-                        },
-                      }}
-                    />
-                  )}
-                </div>
-                <div className="mb-3">
-                  <Input
-                    type="password"
-                    label="Пароль"
-                    placeholder="Придумайте пароль"
-                    name="password"
-                    errors={errorsReg}
-                    register={registerReg}
-                    validation={{
-                      required: "Введите пароль",
-                      minLength: {
-                        value: 6,
-                        message: "Минимальное кол-во символов 6",
-                      },
-                      maxLength: {
-                        value: 250,
-                        message: "Максимальное кол-во символов 250",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="mb-3">
-                  <Input
-                    type="password"
-                    label="Подтверждение пароля"
-                    placeholder="Повторите пароль"
-                    name="passwordConfirm"
-                    errors={errorsReg}
-                    register={registerReg}
-                    validation={{
-                      required: "Введите повторный пароль",
-                      minLength: {
-                        value: 6,
-                        message: "Минимальное кол-во символов 6",
-                      },
-                      maxLength: {
-                        value: 250,
-                        message: "Максимальное кол-во символов 250",
-                      },
-                    }}
-                  />
-                </div>
-                <label className="d-flex pale-blue mb-3">
-                  <input
-                    type="checkbox"
-                    className="checkbox me-2"
-                    {...registerReg("accept", {
-                      required: "Примите условия пользовательского соглашения",
-                    })}
-                  />
-                  <span className="fs-09">
-                    Принять условия Пользовательского соглашения
-                  </span>
-                </label>
-                <Button
-                  type="submit"
-                  variant="primary"
-                  disabled={!isValidReg}
-                  className="w-100 rounded-3"
-                >
-                  Зарегистрироваться
-                </Button>
-              </form>
+                Зарегистрироваться
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => setLoginView(true)}
+                className="main-color fs-13 mx-auto mt-4 text-decoration-underline"
+              >
+                Войти
+              </button>
             )}
-          </div>
-          <div ref={block1} className="login-toggler">
-            <div className="text">
-              <div ref={text1} className="text-1">
-                <h4>Это ваш первый заказ?</h4>
-                <p>Пройдите регистрацию</p>
-              </div>
-              <div ref={text2} className="text-2">
-                <h4>Уже есть аккаунт?</h4>
-                <p>Войди в личный кабинет</p>
-              </div>
+          </section>
+        ) : (
+          <section className="d-none d-lg-block login">
+            <div ref={block2} className="login-forms">
+              {loginView ? <Reg /> : <Login />}
             </div>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-40 rounded-3 mx-auto mt-4"
-            >
-              {loginView ? <span>Регистрация</span> : <span>Войти</span>}
-            </button>
-          </div>
-        </section>
-        <section className="d-lg-none login-mobile">
-          {loginView ? (
-            <button
-              type="button"
-              onClick={() => setLoginView(false)}
-              className="main-color fs-13 mx-auto mt-4 text-decoration-underline"
-            >
-              Зарегистрироваться
-            </button>
-          ) : (
-            <button
-              type="button"
-              onClick={() => setLoginView(true)}
-              className="main-color fs-13 mx-auto mt-4 text-decoration-underline"
-            >
-              Войти
-            </button>
-          )}
-        </section>
+            <div ref={block1} className="login-toggler">
+              <div className="text">
+                <div ref={text1} className="text-1">
+                  <h4>Это ваш первый заказ?</h4>
+                  <p>Пройдите регистрацию</p>
+                </div>
+                <div ref={text2} className="text-2">
+                  <h4>Уже есть аккаунт?</h4>
+                  <p>Войди в личный кабинет</p>
+                </div>
+              </div>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-40 rounded-3 mx-auto mt-4"
+              >
+                {loginView ? <span>Регистрация</span> : <span>Войти</span>}
+              </button>
+            </div>
+          </section>
+        )}
       </Container>
     </main>
   );
