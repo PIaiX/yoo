52e4066ae72518740529beda76b695bef7f50eab
Gikami <gikami.book@gmail.com>
2024-06-14 16:34:21 +0300

    43

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,6 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { Badge, Collapse } from "react-bootstrap";
+import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 import { customPrice, customWeight, getImageURL } from "../helpers/all";
 import ButtonCart from "./ButtonCart";
 // import BtnFav from "./utils/BtnFav";
@@ -12,6 +14,8 @@ const CartItem = memo(({ data }) => {
           data.price
         : data.cart.data.modifiers.reduce((sum, item) => sum + item.price, 0)
       : data.price;
+
+  const [open, setOpen] = useState({ additions: false, wishes: false });
 
   return (
     <div className="cart-item" key={data.id}>
@@ -36,20 +40,78 @@ const CartItem = memo(({ data }) => {
           )}
           {data?.cart?.data?.modifiers?.length > 0 &&
             data.cart.data.modifiers.map((e) => (
-              <p className="fs-09 fw-6">{e.title}</p>
+              <span className="fs-09 fw-7 card d-inline-block p-1 px-2 mb-3 me-2">
+                {e.title}
+              </span>
             ))}
 
           {data?.cart?.data?.additions?.length > 0 && (
-            <>
-              <ul className="cart-item-ingredients">
-                {data.cart.data.additions.map((e) => (
-                  <li>
-                    {e.title}{" "}
-                    <span className="fw-7">+{customPrice(e.price)}</span>
-                  </li>
-                ))}
-              </ul>
-            </>
+            <p>
+              <a
+                className="fs-09 fw-6 d-flex align-items-center mb-0"
+                onClick={() =>
+                  setOpen((prev) => ({ ...prev, additions: !open.additions }))
+                }
+                aria-controls="collapse-additions"
+                aria-expanded={open}
+              >
+                <span>Добавки</span>{" "}
+                <Badge bg="secondary" className="mx-2">
+                  {data?.cart?.data?.additions?.length}
+                </Badge>
+                {open.additions ? (
+                  <IoChevronUp color="#666" />
+                ) : (
+                  <IoChevronDown color="#666" />
+                )}
+              </a>
+              <Collapse in={open.additions}>
+                <div id="collapse-additions">
+                  <ul className="cart-item-ingredients">
+                    {data.cart.data.additions.map((e) => (
+                      <li>
+                        {e.title}{" "}
+                        <span className="fw-7">+{customPrice(e.price)}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </Collapse>
+            </p>
+          )}
+          {data?.cart?.data?.wishes?.length > 0 && (
+            <p>
+              <a
+                className="fs-09 fw-6 d-flex align-items-center mb-0"
+                onClick={() =>
+                  setOpen((prev) => ({ ...prev, wishes: !open.wishes }))
+                }
+                aria-controls="collapse-wishes"
+                aria-expanded={open}
+              >
+                <span>Пожелания</span>{" "}
+                <Badge bg="secondary" className="mx-2">
+                  {data?.cart?.data?.wishes?.length}
+                </Badge>
+                {open.wishes ? (
+                  <IoChevronUp color="#666" />
+                ) : (
+                  <IoChevronDown color="#666" />
+                )}
+              </a>
+              <Collapse in={open.wishes}>
+                <div id="collapse-wishes">
+                  <ul className="cart-item-ingredients-minus">
+                    {data.cart.data.wishes.map((e) => (
+                      <li>
+                        {e.title}{" "}
+                        <span className="fw-7">+{customPrice(e.price)}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </Collapse>
+            </p>
           )}
         </div>
       </div>
--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,4 +1,6 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { Badge, Collapse } from "react-bootstrap";
+import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 import { customPrice, customWeight, getImageURL } from "../helpers/all";
 
 const CheckoutProduct = memo(({ data }) => {
@@ -9,6 +11,9 @@ const CheckoutProduct = memo(({ data }) => {
           data.price
         : data.cart.data.modifiers.reduce((sum, item) => sum + item.price, 0)
       : data.price;
+
+  const [open, setOpen] = useState({ additions: false, wishes: false });
+
   return (
     <div className="checkoutProduct d-flex align-items-start">
       <img src={getImageURL({ path: data.medias })} alt={data.title} />
@@ -28,14 +33,74 @@ const CheckoutProduct = memo(({ data }) => {
           </p>
           <p className="checkoutProduct-count">x{data?.cart?.count ?? 1}</p>
         </div>
+        <div className="border-bottom my-2" />
         {data?.cart?.data?.additions?.length > 0 && (
-          <ul className="cart-item-ingredients border-top mt-2 pt-2">
-            {data.cart.data.additions.map((e) => (
-              <li>
-                {e.title} <span className="fw-7">+{customPrice(e.price)}</span>
-              </li>
-            ))}
-          </ul>
+          <p className="mb-1">
+            <a
+              className="fs-09 fw-6 d-flex align-items-center mb-0"
+              onClick={() =>
+                setOpen((prev) => ({ ...prev, additions: !open.additions }))
+              }
+              aria-controls="collapse-additions"
+              aria-expanded={open}
+            >
+              <span>Добавки</span>{" "}
+              <Badge bg="secondary" className="mx-2">
+                {data?.cart?.data?.additions?.length}
+              </Badge>
+              {open.additions ? (
+                <IoChevronUp color="#666" />
+              ) : (
+                <IoChevronDown color="#666" />
+              )}
+            </a>
+            <Collapse in={open.additions}>
+              <div id="collapse-additions">
+                <ul className="cart-item-ingredients">
+                  {data.cart.data.additions.map((e) => (
+                    <li>
+                      {e.title}{" "}
+                      <span className="fw-7">+{customPrice(e.price)}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Collapse>
+          </p>
+        )}
+        {data?.cart?.data?.wishes?.length > 0 && (
+          <p>
+            <a
+              className="fs-09 fw-6 d-flex align-items-center mb-0"
+              onClick={() =>
+                setOpen((prev) => ({ ...prev, wishes: !open.wishes }))
+              }
+              aria-controls="collapse-wishes"
+              aria-expanded={open}
+            >
+              <span>Пожелания</span>{" "}
+              <Badge bg="secondary" className="mx-2">
+                {data?.cart?.data?.wishes?.length}
+              </Badge>
+              {open.wishes ? (
+                <IoChevronUp color="#666" />
+              ) : (
+                <IoChevronDown color="#666" />
+              )}
+            </a>
+            <Collapse in={open.wishes}>
+              <div id="collapse-wishes">
+                <ul className="cart-item-ingredients-minus">
+                  {data.cart.data.wishes.map((e) => (
+                    <li>
+                      {e.title}{" "}
+                      <span className="fw-7">+{customPrice(e.price)}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Collapse>
+          </p>
         )}
       </div>
     </div>
