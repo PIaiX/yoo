5f396765e851794f79d6ddaec8422b831081480c
Gikami <gikami.book@gmail.com>
2024-11-20 14:40:38 +0300

    121

--- src/components/widget/WidgetBanners.jsx
+++ src/components/widget/WidgetBanners.jsx
@@ -3,6 +3,7 @@ import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { getImageURL } from "../../helpers/all";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
 
 const WidgetBanners = memo((data) => {
   if (!data?.items || data?.items?.length === 0) {
@@ -10,17 +11,73 @@ const WidgetBanners = memo((data) => {
   }
 
   return (
-    <section className="sec-1 my-3">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div
-            className={
-              data?.size === "big"
-                ? "col-12"
-                : "col-12 col-md-11 col-lg-9 col-xl-8"
+    <section className="banner-slider my-3 mb-4">
+      <Slider
+        // className="center"
+        centerMode={true}
+        infinite={true}
+        centerPadding="18%"
+        slidesToShow={1}
+        speed={500}
+        dots={true}
+        lazyLoad={true}
+      >
+        {data.items
+          .filter((item) => {
+            if (item?.options?.start && item?.options?.end) {
+              const startDate = new Date(item.options.start);
+              const endDate = new Date(item.options.end);
+              const currentDateTime = new Date();
+
+              return startDate <= currentDateTime && currentDateTime <= endDate;
             }
-          >
-            <Swiper
+
+            return true;
+          })
+          .map((e, index) =>
+            e?.options?.linkType ? (
+              // <Link
+              //   to={
+              //     e?.options?.linkType === "product" &&
+              //     e?.options?.linkValue
+              //       ? "/product/" + e.options.linkValue
+              //       : e?.options?.linkType === "category" &&
+              //         e.options?.linkValue
+              //       ? "/category/" + e.options.linkValue
+              //       : e?.options?.linkType === "categories"
+              //       ? "/categories"
+              //       : e?.options?.linkType === "sale" &&
+              //         e.options?.linkValue
+              //       ? "/promo/" + e.options.linkValue
+              //       : e?.options?.linkType === "sales"
+              //       ? "/promo"
+              //       : ""
+              //   }
+              // >
+              <img
+                src={getImageURL({
+                  path: e?.medias,
+                  type: "banner",
+                  size: "full",
+                })}
+                alt={e?.title}
+                className={data?.size === "big" ? "img-fluid big" : "img-fluid"}
+              />
+            ) : (
+              // </Link>
+              <img
+                src={getImageURL({
+                  path: e?.medias,
+                  type: "banner",
+                  size: "full",
+                })}
+                alt={e?.title}
+                className={data?.size === "big" ? "img-fluid big" : "img-fluid"}
+              />
+            )
+          )}
+      </Slider>
+      {/* <Swiper
               className="main-slider paginated"
               modules={[Pagination, Autoplay]}
               loop={true}
@@ -105,10 +162,7 @@ const WidgetBanners = memo((data) => {
                     )}
                   </SwiperSlide>
                 ))}
-            </Swiper>
-          </div>
-        </div>
-      </div>
+            </Swiper> */}
     </section>
   );
 });
