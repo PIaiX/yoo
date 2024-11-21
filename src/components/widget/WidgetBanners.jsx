1a15ffd7f4c328d9b1da33a6864108c03c355c5b
gikami <gikami.book@gmail.com>
2024-11-21 19:41:44 +0300

    122

--- src/components/widget/WidgetBanners.jsx
+++ src/components/widget/WidgetBanners.jsx
@@ -1,169 +1,103 @@
-import React, { memo } from "react";
-import { Autoplay, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { memo, useState } from "react";
+// import { Autoplay, Pagination } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
 import { getImageURL } from "../../helpers/all";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 
 const WidgetBanners = memo((data) => {
+  const [mouseMoved, setMouseMoved] = useState(false);
+  const navigate = useNavigate();
   if (!data?.items || data?.items?.length === 0) {
     return null;
   }
 
+  const banners = data.items.filter((item) => {
+    if (item?.options?.start && item?.options?.end) {
+      const startDate = new Date(item.options.start);
+      const endDate = new Date(item.options.end);
+      const currentDateTime = new Date();
+
+      return startDate <= currentDateTime && currentDateTime <= endDate;
+    }
+
+    return true;
+  });
+
+  const link = (e) =>
+    navigate(
+      !mouseMoved && e?.options?.linkType === "product" && e?.options?.linkValue
+        ? "/product/" + e.options.linkValue
+        : !mouseMoved &&
+          e?.options?.linkType === "category" &&
+          e.options?.linkValue
+        ? "/category/" + e.options.linkValue
+        : !mouseMoved && e?.options?.linkType === "categories"
+        ? "/categories"
+        : !mouseMoved && e?.options?.linkType === "sale" && e.options?.linkValue
+        ? "/promo/" + e.options.linkValue
+        : !mouseMoved && e?.options?.linkType === "sales"
+        ? "/promo"
+        : ""
+    );
+
   return (
-    <section className="banner-slider my-3 mb-4">
-      <Slider
-        // className="center"
-        centerMode={true}
-        infinite={true}
-        centerPadding="18%"
-        slidesToShow={1}
-        speed={500}
-        dots={true}
-        lazyLoad={true}
-      >
-        {data.items
-          .filter((item) => {
-            if (item?.options?.start && item?.options?.end) {
-              const startDate = new Date(item.options.start);
-              const endDate = new Date(item.options.end);
-              const currentDateTime = new Date();
-
-              return startDate <= currentDateTime && currentDateTime <= endDate;
-            }
-
-            return true;
-          })
-          .map((e, index) =>
-            e?.options?.linkType ? (
-              // <Link
-              //   to={
-              //     e?.options?.linkType === "product" &&
-              //     e?.options?.linkValue
-              //       ? "/product/" + e.options.linkValue
-              //       : e?.options?.linkType === "category" &&
-              //         e.options?.linkValue
-              //       ? "/category/" + e.options.linkValue
-              //       : e?.options?.linkType === "categories"
-              //       ? "/categories"
-              //       : e?.options?.linkType === "sale" &&
-              //         e.options?.linkValue
-              //       ? "/promo/" + e.options.linkValue
-              //       : e?.options?.linkType === "sales"
-              //       ? "/promo"
-              //       : ""
-              //   }
-              // >
-              <img
-                src={getImageURL({
-                  path: e?.medias,
-                  type: "banner",
-                  size: "full",
-                })}
-                alt={e?.title}
-                className={data?.size === "big" ? "img-fluid big" : "img-fluid"}
-              />
-            ) : (
-              // </Link>
-              <img
-                src={getImageURL({
-                  path: e?.medias,
-                  type: "banner",
-                  size: "full",
-                })}
-                alt={e?.title}
-                className={data?.size === "big" ? "img-fluid big" : "img-fluid"}
-              />
-            )
-          )}
-      </Slider>
-      {/* <Swiper
-              className="main-slider paginated"
-              modules={[Pagination, Autoplay]}
-              loop={true}
-              spaceBetween={15}
-              slidesPerView={1}
-              initialSlide={0}
-              loopedSlides={1}
-              centeredSlides={true}
-              speed={750}
-              autoplay={
-                !!data?.autoScroll
-                  ? {
-                      delay: data?.autoScrollSpeed
-                        ? Number(data.autoScrollSpeed)
-                        : 10000,
-                      pauseOnMouseEnter: true,
-                      disableOnInteraction: false,
-                    }
-                  : false
-              }
-              pagination={{ clickable: true }}
-            >
-              {data.items
-                .filter((item) => {
-                  if (item?.options?.start && item?.options?.end) {
-                    const startDate = new Date(item.options.start);
-                    const endDate = new Date(item.options.end);
-                    const currentDateTime = new Date();
-
-                    return (
-                      startDate <= currentDateTime && currentDateTime <= endDate
-                    );
+    <Slider
+      className="banner-slider my-3 mb-4"
+      centerMode={true}
+      infinite={true}
+      // centerPadding="25%"
+      slidesToShow={1}
+      adaptiveHeight={true}
+      slidesToScroll={1}
+      speed={500}
+      dots={true}
+      // lazyLoad={true}
+      swipe
+      variableWidth={true}
+    >
+      {banners.map((e, index) => {
+        return (
+          <>
+            {e?.options?.linkType ? (
+              <Link
+                onMouseMove={() => setMouseMoved(true)}
+                onMouseDown={() => setMouseMoved(false)}
+                onMouseUp={() => link(e)}
+              >
+                <img
+                  key={index}
+                  src={getImageURL({
+                    path: e?.medias,
+                    type: "banner",
+                    size: "full",
+                  })}
+                  alt={e?.title}
+                  className={
+                    data?.size === "big" ? "img-fluid big" : "img-fluid"
                   }
-
-                  return true;
-                })
-                .map((e, index) => (
-                  <SwiperSlide key={index}>
-                    {e?.options?.linkType ? (
-                      <Link
-                        to={
-                          e?.options?.linkType === "product" &&
-                          e?.options?.linkValue
-                            ? "/product/" + e.options.linkValue
-                            : e?.options?.linkType === "category" &&
-                              e.options?.linkValue
-                            ? "/category/" + e.options.linkValue
-                            : e?.options?.linkType === "categories"
-                            ? "/categories"
-                            : e?.options?.linkType === "sale" &&
-                              e.options?.linkValue
-                            ? "/promo/" + e.options.linkValue
-                            : e?.options?.linkType === "sales"
-                            ? "/promo"
-                            : ""
-                        }
-                      >
-                        <img
-                          src={getImageURL({
-                            path: e?.medias,
-                            type: "banner",
-                            size: "full",
-                          })}
-                          alt={e?.title}
-                          className={
-                            data?.size === "big" ? "img-fluid big" : "img-fluid"
-                          }
-                        />
-                      </Link>
-                    ) : (
-                      <img
-                        src={getImageURL({
-                          path: e?.medias,
-                          type: "banner",
-                          size: "full",
-                        })}
-                        alt={e?.title}
-                        className={
-                          data?.size === "big" ? "img-fluid big" : "img-fluid"
-                        }
-                      />
-                    )}
-                  </SwiperSlide>
-                ))}
-            </Swiper> */}
-    </section>
+                />
+              </Link>
+            ) : (
+              <Link to={link(e)}>
+                <img
+                  key={index}
+                  src={getImageURL({
+                    path: e?.medias,
+                    type: "banner",
+                    size: "full",
+                  })}
+                  alt={e?.title}
+                  className={
+                    data?.size === "big" ? "img-fluid big" : "img-fluid"
+                  }
+                />
+              </Link>
+            )}
+          </>
+        );
+      })}
+    </Slider>
   );
 });
 
