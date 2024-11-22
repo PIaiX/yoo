76f4641d0d329951c0b5ab006411ffc92e04108a
Gikami <gikami.book@gmail.com>
2024-11-22 18:24:10 +0300

    Update WidgetBanners.jsx

--- src/components/widget/WidgetBanners.jsx
+++ src/components/widget/WidgetBanners.jsx
@@ -1,16 +1,40 @@
-import React, { memo, useState } from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 // import { Autoplay, Pagination } from "swiper/modules";
 // import { Swiper, SwiperSlide } from "swiper/react";
 import { getImageURL } from "../../helpers/all";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
 const WidgetBanners = memo((data) => {
+  console.log(data);
+  const hasWindow = typeof window !== "undefined";
+  const [mobile, setMobile] = useState(false);
+  const [width, setWidth] = useState(hasWindow ? window.innerWidth : null);
+  let timeOutId = useRef();
+
   const [mouseMoved, setMouseMoved] = useState(false);
-  const navigate = useNavigate();
+
   if (!data?.items || data?.items?.length === 0) {
     return null;
   }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", () => {
+      clearTimeout(timeOutId.current);
+      timeOutId.current = setTimeout(handleResize, 500);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (width !== null && width < 700) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [width]);
 
   const banners = data.items.filter((item) => {
     if (item?.options?.start && item?.options?.end) {
@@ -41,18 +65,24 @@ const WidgetBanners = memo((data) => {
 
   return (
     <Slider
-      className="banner-slider my-3 mb-4"
-      centerMode={true}
+      className={
+        "banner-slider my-3 mb-5" + (banners?.length === 1 ? " container" : "")
+      }
+      centerMode={!mobile}
       infinite={true}
       // centerPadding="25%"
       slidesToShow={1}
       adaptiveHeight={true}
       slidesToScroll={1}
-      speed={500}
-      dots={true}
+      autoplay={data?.autoScroll}
+      autoplaySpeed={
+        data?.autoScrollSpeed ? Number(data.autoScrollSpeed) : 5000
+      }
+      pauseOnHover={true}
+      dots={banners?.length > 1}
       // lazyLoad={true}
-      swipe
-      variableWidth={true}
+      swipe={true}
+      variableWidth={!mobile && banners?.length > 1}
     >
       {banners.map((e, index) => {
         return (
